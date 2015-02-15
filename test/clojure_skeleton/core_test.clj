76bf5f9cf892f9f18a38876c810ab959b58309f0
Alexander Reitzel <funtimecoding@gmail.com>
2015-02-15 13:43:10 +0100

    add lein skeleton files

---
+++ test/clojure_skeleton/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-skeleton.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-skeleton.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
